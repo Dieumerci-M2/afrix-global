4bb1e7cbbc498f670071d462daa91b418edc4967
Tacite243 <taciteiragi@gmail.com>
2025-12-02 14:10:17 +0200

    feat(global): update site structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+  return (
+    <header>
+      <h1>This is header of our site</h1>
+    </header>
+  );
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+
+
+export default function HeroSection() {
+  return (
+    <section>
+      <h2>Welcome to the Hero Section</h2>
+    </section>
+  );
+}
