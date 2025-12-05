d5306739f5209ec291e569816bcc712e72f0942d
laurence1Mas <119684303+laurence1Mas@users.noreply.github.com>
2025-12-05 15:39:49 +0100

    Feat: Service section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
+import ServiceSection from '../components/ServiceSection'
+
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection/>
       <AboutSection/>
+      <ServiceSection/>
     </main>
   );
 }
